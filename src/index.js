0b8af91d0087ec7c82f6b2a8749521a489f239bc
Maria Souza <messenger.mariah2017@gmail.com>
2020-08-06 00:50:26 -0300

    novos componentes e rotas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import  { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/App';
+import CadastroVideo from "./pages/cadastro/videos"
+import cadastroCategoria from "./pages/cadastro/Categoria"
 
+
+
+
+
+
+const Pagina404 = ()=> (<div>Página 404</div>)
+
+ 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path="/" component={Home} exact />  
+    <Route path="/cadastro/video" component={CadastroVideo}/>
+    <Route path="/cadastro/categoria" component={cadastroCategoria}/>
+    <Route component={Pagina404}/>
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
